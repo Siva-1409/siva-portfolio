b1a53aca244fee8f676cdb071ef6318a67ed420d
Siva-1409 <sivakumarkm123456@gmail.com>
2026-06-08 21:45:05 +0530

    Fixed homepage route

--- server.js
+++ server.js
@@ -55,6 +55,10 @@ app.get("/projects", (req, res) => {
 
 });
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
